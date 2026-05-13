aba0c6dc0674149ad8eb46bc2b3696dd49cd59e6
Darius <darius.cotiata@student.tuiasi.ro>
2026-05-13 19:12:24 +0300

    Working code for left turns

--- Code/traseuVirajat.v
+++ Code/traseuVirajat.v
@@ -1,157 +1,15 @@
-/*module traseuVirajat(Sensors, Clk, EN1A, EN2A, EN1B, EN2B, IN1A, IN2A, IN3A, IN4A, IN1B, IN2B, IN3B, IN4B);
-
-	localparam baseSpeed = 100;
-
-	input Clk;
-	input[4:0] Sensors;
-	output wire EN1A, EN2A, EN1B, EN2B;
-	output wire IN1A, IN2A, IN3A, IN4A, IN1B, IN2B, IN3B, IN4B;
-	reg[7:0] leftSpeed, rightSpeed;
-	reg leftTriggered, rightTriggered;
-	
-	
-	assign IN1A = 1'b0;
-    assign IN2A = 1'b1;
-    assign IN3A = 1'b0;
-    assign IN4A = 1'b1;
-    assign IN1B = 1'b0;
-    assign IN2B = 1'b1;
-    assign IN3B = 1'b0;
-    assign IN4B = 1'b1;
-    
-    
-    Motors_Speed ms0(EN1A, Clk, leftSpeed);
-    Motors_Speed ms1(EN2A, Clk, rightSpeed);
-    Motors_Speed ms2(EN1B, Clk, rightSpeed);
-    Motors_Speed ms3(EN2B, Clk, leftSpeed);
-        
-    initial begin
-		
-		leftSpeed      = baseSpeed;
-		rightSpeed     = baseSpeed;
-		leftTriggered = 0;
-		rightTriggered = 0;
-		
-    end
-    
-// Block 2 - clocked, applies correction to speed after PID has computed
-/*always @(Sensors) begin
-    case(Sensors)
-        5'b00100: begin
-			if(leftTriggered == 1'b1) begin
-			
-			leftSpeed = baseSpeed - 50;
-            rightSpeed = baseSpeed + 20;
-        
-        end
-			else begin
-            leftSpeed = baseSpeed;
-            rightSpeed = baseSpeed;
-            leftTriggered = 1'b0;
-
-            end
-        end
-        5'b00101: begin
-        
-            leftSpeed  = baseSpeed;
-            rightSpeed = baseSpeed;
-            leftTriggered = 1'b1;
-            
-        end
-        
-        5'b00000: begin
-            if(leftTriggered == 1'b1) begin
-            
-				leftSpeed = baseSpeed - 50;
-				rightSpeed = baseSpeed + 20;
-            
-            end
-            else begin
-            
-				leftSpeed = 0;
-				rightSpeed = 0;
-				leftTriggered = 1'b0;
-
-				end
-        end
-        
-        5'b01000: begin
-	
-			leftSpeed = baseSpeed + 50;
-			rightSpeed = baseSpeed - 30;
-			leftTriggered = 1'b0;
-	
-		end
-        
-        
-        default: begin
-            if(leftTriggered == 1'b1) begin
-			leftSpeed = baseSpeed - 50;
-            rightSpeed = baseSpeed + 20;
-        end
-        else begin
-			leftSpeed = baseSpeed;
-            rightSpeed = baseSpeed;
-            leftTriggered = 1'b0;
-        end
-        end
-    endcase
-end
-
-always @(Sensors) begin
-    case(Sensors)
-        5'b00100: error = 0;
-        5'b00110: error = -1;
-        5'b00010: error = -2;
-        5'b00001: error = -3;
-        5'b01100: error = 1;
-        5'b01000: error = 2;
-        5'b10000: error = 3;
-        5'b10100: error = 0;
-        5'b00101: error = 0;
-        default:  error = lastError;
-    endcase
-    lastError = error;
-end
-
-// Block 2 - clocked, applies correction to speed after PID has computed
-always @(posedge Clk) begin
-    case(Sensors)
-        5'b10100: begin
-            leftSpeed  <= 160 + correction;
-            rightSpeed <= 160 - correction;
-        end
-        5'b00101: begin
-            leftSpeed  <= 160 + correction;
-            rightSpeed <= 160 - correction;
-        end
-        5'b00000: begin
-            leftSpeed  <= 120;
-            rightSpeed <= 120;
-        end
-        default: begin
-            leftSpeed  <= baseSpeed + correction;
-            rightSpeed <= baseSpeed - correction;
-        end
-    endcase
-
-    if(leftSpeed < 120)  leftSpeed  <= 120;
-    if(rightSpeed < 120) rightSpeed <= 120;
-    if(leftSpeed > 255)  leftSpeed  <= 255;
-    if(rightSpeed > 255) rightSpeed <= 255;
-end
-    
-endmodule*/
-
 module traseuVirajat(Sensors, Clk, EN1A, EN2A, EN1B, EN2B, IN1A, IN2A, IN3A, IN4A, IN1B, IN2B, IN3B, IN4B);
-    localparam baseSpeed = 100;
+    localparam baseSpeed = 150;
     input Clk;
     input[4:0] Sensors;
     output wire EN1A, EN2A, EN1B, EN2B;
     output wire IN1A, IN2A, IN3A, IN4A, IN1B, IN2B, IN3B, IN4B;
     reg[7:0] leftSpeed, rightSpeed;
     reg signed[2:0] error, lastError;
-    wire signed[8:0] correction;
+    wire signed[7:0] correction;
+    reg[2:0] Trigger;
+reg signed [9:0] rawLeft, rawRight; 
+
 
     /* Forward movement */
     assign IN1A = 1'b0;
@@ -177,47 +35,68 @@ module traseuVirajat(Sensors, Clk, EN1A, EN2A, EN1B, EN2B, IN1A, IN2A, IN3A, IN4
         rightSpeed = baseSpeed;
         error      = 0;
         lastError  = 0;
+        Trigger = 2'b00;
     end
 
     /* Error from sensors - combinational */
     always @(Sensors) begin
         case(Sensors)
-            5'b00100: error = 0;				//b?010?
-            5'b00110: error = -1;				//b?011?
-            5'b00010: error = -2;				//b?001?
-            5'b00001: error = -3;
-            5'b01100: error = 1;				//b?110?
-            5'b01000: error = 2;				//b?100?
-            5'b10000: error = 3;
-            5'b10100: error = 0;
-            5'b00101: error = 0;
+            5'b00100: error = 0;
+            5'b00110: error = -1;
+            5'b00010: error = -2;
+            5'b01100: error = 1;
+            5'b01000: error = 2;
             default:  error = lastError;
         endcase
         lastError = error;
     end
 
-    /* Speed assignment - clocked */
-   always @(posedge Clk) begin
-    reg signed[8:0] leftRaw, rightRaw;
-    
-    leftRaw  = 9'sd100 + correction;
-	rightRaw = 9'sd100 - correction;
-    
-    case(Sensors)
-        5'b10100, 5'b00101: begin
-            leftSpeed  <= (leftRaw  < 50) ? 50 : (leftRaw  > 250) ? 250 : leftRaw[7:0];
-            rightSpeed <= (rightRaw < 50) ? 50 : (rightRaw > 250) ? 250 : rightRaw[7:0];
-        end
-        5'b00000: begin
-            leftSpeed  <= 80;
-            rightSpeed <= 80;
-        end
-        default: begin
-            leftSpeed  <= (leftRaw  < 50) ? 50 : (leftRaw  > 250) ? 250 : leftRaw[7:0];
-            rightSpeed <= (rightRaw < 50) ? 50 : (rightRaw > 250) ? 250 : rightRaw[7:0];
-        end
-    endcase
-end
-     
+    /* Unified Speed Assignment & Trigger Sequence */
+    always @(posedge Clk) begin
+        
+        // ==========================================
+        // 1. UPDATE THE TRIGGER MEMORY
+        // ==========================================
+        case (Trigger)
+            2'b000: begin // IDLE
+                if (Sensors == 5'b10100) Trigger <= 2'b001; // ARM!
+            end
+            2'b001: begin // ARMED
+                if (Sensors == 5'b00100) Trigger <= 2'b010; // FIRE! (Marker ended)
+            end
+            3'b010: begin // YANK LEFT (Waiting for the line to leave the center)
+                // If the sensors are no longer reading the center 3 positions, move to State 11
+                if (Sensors != 5'b00100 && Sensors != 5'b01100 && Sensors != 5'b00110) 
+                    Trigger <= 2'b011; 
+            end
+            3'b011: begin // CATCH CURVE (Waiting to find line again)
+                // Once the line swings back into any of the center 3 positions, the turn is done!
+                if (Sensors == 5'b01100 || Sensors == 5'b00110 || Sensors == 5'b00100) 
+                    Trigger <= 3'b000; // Back to PID!
+            end
+        endcase
 
+        if (Trigger == 3'b010 || Trigger == 3'b011) begin
+            leftSpeed  <= 0;  
+            rightSpeed <= 255; 
+        end
+        
+        else if (Trigger == 3'b001) begin
+            leftSpeed  <= baseSpeed;
+            rightSpeed <= baseSpeed;
+        end
+        
+        else begin
+            rawLeft  = baseSpeed + correction;
+            rawRight = baseSpeed - correction;
+            
+            if(rawLeft < 0)       leftSpeed <= 0;
+            else if(rawLeft > 255) leftSpeed <= 255;
+            else                   leftSpeed <= rawLeft[7:0];
+
+            if(rawRight < 0)       rightSpeed <= 0;
+            else if(rawRight > 255) rightSpeed <= 255;
+            else                    rightSpeed <= rawRight[7:0];
+        end
+    end
 endmodule
